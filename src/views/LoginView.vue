526338e335bf8b92c32903fa0ca0395f7a67febc
brunahenz <brunaehenz@gmail.com>
2024-07-16 16:17:54 -0300

    Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="o1GfrdWckD6A98EO4ouJL6zy"
+    ></passage-auth>
+  </div>
 </template>
